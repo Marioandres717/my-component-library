dd7f7f5f7a078cdf446d0f7fa311801e7caccefc
Mario Andres <marioarendon2@gmail.com>
2020-11-11 15:00:19 -0600

    add button status

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -8,22 +8,59 @@ const Button = styled.button`
   min-width: 100px;
   cursor: pointer;
   font-family: ${defaultTheme.primaryFont};
+  transition: background-color 0.2s linear, color 0.2s linear;
+
+  &:hover {
+    background-color: ${defaultTheme.primaryColorHover};
+    color: ${defaultTheme.textColorOnPrimary};
+  }
+
+  &:focus {
+    outline: 3px solid ${defaultTheme.primaryColorFocus};
+    outline-offset: 2px;
+    background-color: ${defaultTheme.primaryColorFocus};
+    color: ${defaultTheme.textColorOnPrimary};
+  }
+
+  &:active {
+    background-color: ${defaultTheme.primaryColorActive};
+    border-color: ${defaultTheme.primaryColorActive};
+    color: ${defaultTheme.textColorOnPrimary};
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+  }
 `;
 
 const PrimaryButton = styled(Button)`
   background-color: ${defaultTheme.primaryColor};
   color: ${defaultTheme.textColorOnPrimary};
   border: 2px solid transparent;
+  &:disabled {
+    background-color: ${defaultTheme.primaryColorDisabled};
+    color: ${defaultTheme.textColorOnDisabled};
+  }
 `;
 
 const SecondaryButton = styled(Button)`
-  border-color: ${defaultTheme.primaryColor};
+  border: 2px solid ${defaultTheme.primaryColor};
   color: ${defaultTheme.primaryColor};
+
+  &:disabled {
+    background: none;
+    color: ${defaultTheme.primaryColorDisabled};
+    border-color: ${defaultTheme.primaryColorDisabled};
+  }
 `;
 
 const TertiaryButton = styled(Button)`
   border: 2px solid transparent;
   color: ${defaultTheme.primaryColor};
+  &:disabled {
+    background: none;
+    color: ${defaultTheme.primaryColorDisabled};
+  }
 `;
 
 export { PrimaryButton, SecondaryButton, TertiaryButton };
--- src/utils/themes.js
+++ src/utils/themes.js
@@ -4,10 +4,13 @@ import { primaryFont } from "./typography";
 const defaultTheme = {
   primaryColor: purple[300],
   primaryColorHover: purple[200],
+  primaryColorFocus: purple[200],
   primaryColorActive: purple[100],
+  primaryColorDisabled: neutral[400],
   textColorOnPrimary: neutral[100],
   textColor: neutral[100],
   textColorInverted: neutral[600],
+  textColorOnDisabled: neutral[100],
   primaryFont,
 };
 
