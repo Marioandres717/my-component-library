3b128b3bf1222fe2d93553e8c10a8598831320a1
Mario Andres <marioarendon2@gmail.com>
2020-11-11 00:52:16 -0600

    add theme

---
+++ src/utils/themes.js
@@ -0,0 +1,14 @@
+import { purple, neutral } from "./colors";
+import { primaryFont } from "./typography";
+
+const defaultTheme = {
+  primaryColor: purple[300],
+  primaryColorHover: purple[200],
+  primaryColorActive: purple[100],
+  textColorOnPrimary: neutral[100],
+  textColor: neutral[100],
+  textColorInverted: neutral[600],
+  primaryFont,
+};
+
+export default defaultTheme;
