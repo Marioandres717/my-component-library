d77a09e1ea695b4c02c9779bf95022a40b3cf182
Mario Andres <marioarendon2@gmail.com>
2020-11-11 18:25:31 -0600

    add status css

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,6 +1,63 @@
 import styled from "styled-components";
 import { typeScale, defaultTheme } from "../utils";
+import { applyStyleModifiers } from "styled-components-modifiers";
 
+const BUTTON_MODIFIERS = {
+  small: () => `
+    font-size: ${typeScale.helperText};
+    padding: 8px;
+  `,
+  large: () => `
+    font-size: ${typeScale.h5};
+    padding: 16px 24px;
+  `,
+  warning: () => `
+    color: ${defaultTheme.textColor};
+    border-color: ${defaultTheme.textColor};
+    background: none;
+    background-color: ${defaultTheme.status.warningColor};
+    &:hover, &:focus {
+      background-color: ${defaultTheme.status.warningColorHover};
+      outline: 3px solid ${defaultTheme.status.warningColorHover};
+      outline-offset: 2px;
+      border: 2px solid transparent;
+    }
+
+    &:active {  
+      background-color: ${defaultTheme.status.warningColorActive};
+    }
+  `,
+  error: () => `
+    color: ${defaultTheme.textColor};
+    background: none;
+    background-color: ${defaultTheme.status.errorColor};
+    &:hover, &:focus {
+      background-color: ${defaultTheme.status.errorColorHover};
+      outline: 3px solid ${defaultTheme.status.errorColorHover};
+      outline-offset: 2px;
+      border: 2px solid transparent;
+    }
+
+    &:active {
+      background-color: ${defaultTheme.status.errorColorActive};
+    }
+  `,
+  success: () => `
+    color: ${defaultTheme.textColor};
+    background: none;
+    background-color: ${defaultTheme.status.successColor};
+    &:hover, &:focus {
+      background-color: ${defaultTheme.status.successColorHover};
+      outline: 3px solid ${defaultTheme.status.successColorHover};
+      outline-offset: 2px;
+      border: 2px solid transparent;
+    }
+
+    &:active {
+      background-color: ${defaultTheme.status.successColorActive};
+    }
+  `,
+};
 const Button = styled.button`
   padding: 12px 25px;
   font-size: ${typeScale.paragraph};
@@ -41,6 +98,8 @@ const PrimaryButton = styled(Button)`
     background-color: ${defaultTheme.primaryColorDisabled};
     color: ${defaultTheme.textColorOnDisabled};
   }
+
+  ${applyStyleModifiers(BUTTON_MODIFIERS)}
 `;
 
 const SecondaryButton = styled(Button)`
@@ -52,6 +111,8 @@ const SecondaryButton = styled(Button)`
     color: ${defaultTheme.primaryColorDisabled};
     border-color: ${defaultTheme.primaryColorDisabled};
   }
+
+  ${applyStyleModifiers(BUTTON_MODIFIERS)}
 `;
 
 const TertiaryButton = styled(Button)`
@@ -61,6 +122,8 @@ const TertiaryButton = styled(Button)`
     background: none;
     color: ${defaultTheme.primaryColorDisabled};
   }
+
+  ${applyStyleModifiers(BUTTON_MODIFIERS)}
 `;
 
 export { PrimaryButton, SecondaryButton, TertiaryButton };
--- src/utils/themes.js
+++ src/utils/themes.js
@@ -1,4 +1,4 @@
-import { purple, neutral } from "./colors";
+import { purple, neutral, yellow, green, red } from "./colors";
 import { primaryFont } from "./typography";
 
 const defaultTheme = {
@@ -12,6 +12,17 @@ const defaultTheme = {
   textColorInverted: neutral[600],
   textColorOnDisabled: neutral[100],
   primaryFont,
+  status: {
+    warningColor: yellow[100],
+    warningColorHover: yellow[200],
+    warningColorActive: yellow[300],
+    errorColor: red[100],
+    errorColorHover: red[200],
+    errorColorActive: red[300],
+    successColor: green[100],
+    successColorHover: green[200],
+    successColorActive: green[300],
+  },
 };
 
 export { defaultTheme };
